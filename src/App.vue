aecfb83d5048adf6f210dec9098911d081affd31
tsvoboda-wake <taylorsmithfromm+wake@gmail.com>
2025-11-02 18:49:28 -0500

    feat: vue js app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Random Quote Generator</h1>
+
+    <!-- Display the quote or a loading message -->
+    <p v-if="loading">Loading...</p>
+    <p v-else>"{{ quote }}"</p>
+
+    <!-- The button that triggers the API call -->
+    <button @click="getQuote">Get New Quote</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      quote: '',      // stores the quote text
+      loading: false  // tracks loading status
+    }
+  },
+  methods: {
+    async getQuote() {
+      this.loading = true
+      try {
+        // Send a GET request to the Quotable API
+        const response = await axios.get('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+          headers: {
+            'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+            'x-rapidapi-key': '72697e8fc4msh8caf613dab3491cp15c72ajsn8bd08a2751d9'
+            }
+          });
+        // Update the quote with the response data
+        this.quote = response.data.content;
+        this.author = response.data.originator.name; 
+      } catch (error) {
+        console.error('Error fetching quote:', error)
+        this.quote = 'Oops! Something went wrong.'
+        this.author = ''
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  mounted() {
+    // Load a random quote when the app first starts
+    this.getQuote()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Arial, sans-serif;
+  margin-top: 50px;
 }
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+.author {
+  font-style: italic;
+  color: #555;
+  margin-top: 5px;
+}
+
 </style>
+
